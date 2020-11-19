c0f4f13e592f659a0a1cb7972d880a89c8df87f1
unknown <simun.terzanovic@gmail.com>
2020-11-19 13:45:04 +0100

    modify db-init

--- util/db-init.sql
+++ util/db-init.sql
@@ -1,24 +1,23 @@
 SET FOREIGN_KEY_CHECKS = 0;
-DROP TABLE IF EXISTS iwi.users;
-DROP TABLE IF EXISTS iwi.coordinates;
+DROP TABLE IF EXISTS `heroku_7c819fe7f578379`.users;
+DROP TABLE IF EXISTS `heroku_7c819fe7f578379`.coordinates;
 SET FOREIGN_KEY_CHECKS = 1;
 
-CREATE TABLE IF NOT EXISTS iwi.users (
+CREATE TABLE IF NOT EXISTS `heroku_7c819fe7f578379`.users (
 	id INT(10) NOT NULL AUTO_INCREMENT,
     username VARCHAR(50),
     email VARCHAR(100),
     PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS iwi.coordinates (
-	userId INT(10) NOT NULL,
+CREATE TABLE IF NOT EXISTS `heroku_7c819fe7f578379`.coordinates (
+	userId INT(10) NOT NULL UNIQUE,
 	latitude DECIMAL(9,6),
     longitude DECIMAL(9,6),
-    PRIMARY KEY (userId),
-    FOREIGN KEY (userId) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (userId) REFERENCES `heroku_7c819fe7f578379`.users (id)
 );
 
-INSERT INTO users(username, email) VALUES
+INSERT INTO `heroku_7c819fe7f578379`.users(username, email) VALUES
 ('ana', 'ana@gmail.com'),
 ('marko', 'marko@gmail.com'),
 ('ivana', 'ivana@hotmail.com'),
@@ -30,14 +29,14 @@ INSERT INTO users(username, email) VALUES
 ('zorana', 'zorka55@yahoo.com'),
 ('biserka', 'bisa@gmail.com');
 
-INSERT INTO coordinates(userId, latitude, longitude) VALUES
+INSERT INTO `heroku_7c819fe7f578379`.coordinates(userId, latitude, longitude) VALUES
 (1, 45.813347, 15.976617),
-(2, 45.813298, 15.976376),
-(3, 45.813569, 15.979577),
-(4, 45.814599, 15.978603),
-(5, 45.805076, 15.978091),
-(6, 45.810657, 15.987553),
-(7, 45.815916, 15.992884),
-(8, 45.826004, 16.108529),
-(9, 45.824742, 16.036700),
-(10, 45.824999, 16.021449);
+(11, 45.813298, 15.976376),
+(21, 45.813569, 15.979577),
+(31, 45.814599, 15.978603),
+(41, 45.805076, 15.978091),
+(51, 45.810657, 15.987553),
+(61, 45.815916, 15.992884),
+(71, 45.826004, 16.108529),
+(81, 45.824742, 16.036700),
+(91, 45.824999, 16.021449);
